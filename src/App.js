10a9b3370ef058aadad06c4492131fff398d4c03
Ben DiFrancesco <ben@scopelift.co>
2018-04-11 17:10:29 -0400

    Update App.js to pull the HodlWallet deploy date from the contract

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
+import HodlWalletContract from '../build/contracts/HodlWallet.json'
 import getWeb3 from './utils/getWeb3'
 
 import './css/oswald.css'
@@ -13,6 +14,8 @@ class App extends Component {
 
 	this.state = {
 	    storageValue: 0,
+	    deployDate: 0,
+	    withdrawDate: 0,
 	    web3: null
 	}
     }
@@ -27,21 +30,31 @@ class App extends Component {
 		    web3: results.web3
 		})
 
-		// Instantiate contract once web3 provided.
-		this.instantiateContract()
+		this.instantiateHodlWallet();
 	    })
 	    .catch(() => {
 		console.log('Error finding web3.')
 	    })
     }
 
+    instantiateHodlWallet() {
+	const contract = require('truffle-contract');
+	const hodlWallet = contract(HodlWalletContract);
+	hodlWallet.setProvider(this.state.web3.currentProvider);
+	
+	var hodlWalletInstance;
+
+	this.state.web3.eth.getAccounts( (error, accounts) => {
+	    hodlWallet.deployed().then( (instance) => {
+		hodlWalletInstance = instance;
+		return hodlWalletInstance.getDeployDate.call(accounts[0]);
+	    }).then( (result) => {
+		return this.setState({ deployDate: result.c[0] })
+	    })
+	})
+    }
+
     instantiateContract() {
-	/*
-	 * SMART CONTRACT EXAMPLE
-	 *
-	 * Normally these functions would be called in the context of a
-	 * state management library, but for convenience I've placed them here.
-	 */
 
 	const contract = require('truffle-contract')
 	const simpleStorage = contract(SimpleStorageContract)
@@ -82,7 +95,7 @@ class App extends Component {
 		<h2>Smart Contract Example</h2>
 		<p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
 		<p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
-		<p>The stored value is: {this.state.storageValue}</p>
+		<p>The stored value is: {this.state.deployDate}</p>
 		</div>
 		</div>
 		</main>
