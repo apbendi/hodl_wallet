12f90897a70d69cae982fb3baadb77c3ad43e20b
Ben DiFrancesco <ben@scopelift.co>
2018-04-13 15:53:44 -0400

    Remove references to SimpleStorage example from Truffle unboxing

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 var HodlWallet = artifacts.require("./HodlWallet.sol");
 
 module.exports = function(deployer) {
-    deployer.deploy(SimpleStorage);
     deployer.deploy(HodlWallet, 0);
 };
--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
 import HodlWalletContract from '../build/contracts/HodlWallet.json'
 import getWeb3 from './utils/getWeb3'
 
@@ -27,9 +26,6 @@ class App extends Component {
     }
 
     componentWillMount() {
-	// Get network provider and web3 instance.
-	// See utils/getWeb3 for more info.
-
 	getWeb3
 	    .then(results => {
 		this.setState({
@@ -91,32 +87,6 @@ class App extends Component {
 
     handleDepositClick() {
 	this.deposit(5000000000000000000);
-    }
-
-    instantiateContract() {
-
-	const contract = require('truffle-contract')
-	const simpleStorage = contract(SimpleStorageContract)
-	simpleStorage.setProvider(this.state.web3.currentProvider)
-
-	// Declaring this for later so we can chain functions on SimpleStorage.
-	var simpleStorageInstance
-
-	// Get accounts.
-	this.state.web3.eth.getAccounts((error, accounts) => {
-	    simpleStorage.deployed().then((instance) => {
-		simpleStorageInstance = instance
-
-		// Stores a given value, 5 by default.
-		return simpleStorageInstance.set(116, {from: accounts[0]})
-	    }).then((result) => {
-		// Get the value from the contract to prove it worked.
-		return simpleStorageInstance.get.call(accounts[0])
-	    }).then((result) => {
-		// Update state with the result.
-		return this.setState({ storageValue: result.c[0] })
-	    })
-	})
     }
 
     render() {
